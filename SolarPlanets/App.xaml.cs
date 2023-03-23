6309ac121d1e5b51d8cd2b89fba0d699b5cba22a
Opilane TTHK <andreluige@hot.ee>
2023-03-23 14:51:31 +0200

    Setting default window size for app

--- SolarPlanets/App.xaml.cs
+++ SolarPlanets/App.xaml.cs
@@ -1,10 +1,33 @@
-﻿namespace SolarPlanets;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+using WinRT.Interop;
+#endif
+
+namespace SolarPlanets;
 
 public partial class App : Application
 {
+	const int WindowWidth = 540;
+	const int WindowHeight = 1000;
+
 	public App()
 	{
 		InitializeComponent();
+
+#if WINDOWS
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+            var mauiWindow = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowHandle = WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+			AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        });
+#endif
 
 		MainPage = new AppShell();
 	}
